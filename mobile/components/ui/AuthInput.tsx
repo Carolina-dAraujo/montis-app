ba1019e0213560f7c5b1a864b3f38f8f72d856a0
carolaraujo <anacarolinadearaujonunes@gmail.com>
2025-05-17 03:13:24 -0300

    styling the register and login screens

--- mobile/components/ui/AuthInput.tsx
+++ mobile/components/ui/AuthInput.tsx
@@ -1,35 +1,50 @@
 import React from 'react';
-import { StyleSheet, TextInput, TextInputProps } from 'react-native';
+import {StyleSheet, Text, TextInput, TextInputProps, View} from 'react-native';
 import { Colors } from '@/mobile/constants/Colors';
 
 interface AuthInputProps extends TextInputProps {
+    label: string;
     placeholder: string;
     value: string;
     onChangeText: (text: string) => void;
     secureTextEntry?: boolean;
 }
 
-export const AuthInput: React.FC<AuthInputProps> = ({placeholder, value, onChangeText, secureTextEntry, ...rest}) => (
-    <TextInput
-        style={styles.input}
-        placeholder={placeholder}
-        placeholderTextColor={Colors.light.text}
-        value={value}
-        onChangeText={onChangeText}
-        secureTextEntry={secureTextEntry}
-        {...rest}
-    />
+export const AuthInput: React.FC<AuthInputProps> = ({label, placeholder, value, onChangeText, secureTextEntry, ...rest}) => (
+    <View style={styles.container}>
+        <Text style={styles.label}>{label}</Text>
+        <TextInput
+            style={styles.input}
+            placeholder={placeholder}
+            placeholderTextColor={Colors.light.text}
+            value={value}
+            onChangeText={onChangeText}
+            secureTextEntry={secureTextEntry}
+            {...rest}
+        />
+    </View>
 );
 
 const styles = StyleSheet.create({
     input: {
-        height: 48,
+        height: 38,
         borderColor: Colors.light.icon,
         borderWidth: 1,
         borderRadius: 8,
         paddingHorizontal: 12,
-        marginBottom: 12,
+        marginBottom: 1,
         backgroundColor: '#fff',
         color: Colors.light.text,
+    },
+    container: {
+        marginBottom: 2,
+    },
+    label: {
+        fontSize: 10,
+        fontWeight: '500',
+        fontFamily: 'Roboto',
+        color: Colors.light.text,
+        marginBottom: 4,
+        textAlign: 'left',
     },
 });
